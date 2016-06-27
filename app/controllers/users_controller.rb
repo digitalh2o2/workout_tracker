739280f3da07b954268b316346869e4bad10e3db
Saul Ocampo <digitalh2o@gmail.com>
2016-06-26 22:27:21 -0500

    Workouts can be created and displayed. Working on format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,24 @@ class UsersController < ApplicationController
 			redirect to '/workouts'
 		end
 	end
+
+	post '/login' do
+		@user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			@user.save
+			session[:user_id] = @user.id
+
+			flash[:message] = "You have successfully logged in"
+
+			redirect to '/workouts'
+		else
+			flash[:message] = "Username and/or password is invalid. Please try again"
+			redirect to '/login'
+		end
+	end
+
+	get '/logout' do
+		session.clear
+		redirect to '/'
+	end
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,3 +1,36 @@
 class WorkoutsController < ApplicationController
 
+	get '/workouts' do
+		if !session[:user_id]
+			redirect to '/login'
+		else
+			@user = User.find(session[:user_id])
+			erb :'/workouts/workouts'
+		end
+	end
+
+	get '/workouts/new' do
+		if !session[:user_id]
+			redirect to '/login'
+		else
+			@user = User.find(session[:user_id])
+			erb :'/workouts/new_workout'
+		end
+	end
+
+	post '/workouts' do
+		if params[:name].empty? || params[:date].empty?
+			flash[:message] = "Workout needs a name and/or date."
+
+			redirect to "/workouts/new"
+		else
+			@workout = Workout.create(name: params[:name], pounds: params[:pounds], repetitions: params[:repetitions], sets: params[:sets], muscle_group: params[:muscle_group], date: params[:date])
+			@workout.user_id = session[:user_id]
+			@workout.save
+
+			flash[:message] = "You have logged a new workout!"
+
+			redirect to '/workouts'
+		end
+	end
 end
