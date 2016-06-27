d8f408c854666f68fc0b91a67ba687b7a9a57c62
Saul Ocampo <digitalh2o@gmail.com>
2016-06-26 19:00:17 -0500

    Create relational tables

---
+++ db/migrate/20160626234554_create_workouts.rb
@@ -0,0 +1,12 @@
+class CreateWorkouts < ActiveRecord::Migration
+  def change
+  	create_table :workouts do |t|
+  		t.string :name
+  		t.integer :repetitions
+  		t.integer :sets
+  		t.string :muscle_group
+  		t.string :date
+  		t.integer :user_id
+  	end
+  end
+end
